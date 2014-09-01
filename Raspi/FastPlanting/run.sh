e3c8ea9887e16f6b3f08e4c3484e3470cb7b30f0
Kun Ling <timonkun@gmail.com>
2014-09-01 22:29:50 +0800

    Update script.

--- Raspi/FastPlanting/run.sh
+++ Raspi/FastPlanting/run.sh
@@ -2,18 +2,20 @@
 
 log_file="./cron.log"
 
+cd /home/pi/Workspace/Fast_Planting_System/Raspi/FastPlanting
+
 while : 
 do 
     echo "Current DIR is " $PWD 
-    stillRunning=$(ps -ef |grep "$PWD/loader" |grep -v "grep") 
+    stillRunning=$(ps -ef |grep "$PWD/fastplanting_main.py" | grep -v "grep") 
     if [ "$stillRunning" ] ; then 
         echo "TWS service was already started by another way" 
         echo "Kill it and then startup by this shell, other wise this shell will loop out this message annoyingly" 
-        kill -9 $pidof $PWD/loader 
+        kill -9 $pidof $PWD/fastplanting_main.py 
     else 
         echo "TWS service was not started" 
         echo "Starting service ..." 
-        $PWD/loader $log_file
+        python $PWD/fastplanting_main.py 2>>$log_file
         echo "TWS service was exited at `date`" >> $log_file
     fi 
     sleep 10 
